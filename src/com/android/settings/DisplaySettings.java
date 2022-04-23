152e68b434a4213f066e8cac2b4c634d164f09da
sairam1411 <majgaonkars3@gmail.com>
2022-04-23 20:10:24 +0530

    Settings: Add back support for changing icon shapes
    Signed-off-by: sairam1411 <majgaonkars3@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -130,6 +130,8 @@ public class DisplaySettings extends DashboardFragment implements
                 "android.theme.customization.icon_pack"));
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.font"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.adaptive_icon_shape"));
         return controllers;
     }
 
