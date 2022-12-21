d52bb9a4751634488f677eb2408bf6e46f74d352
Rush-er <stoccomatis7@gmail.com>
2022-12-21 10:56:05 +0100

    Revert "Keyguard: Forward port lockscreen quick unlock (2/2)"
    This reverts commit 63e1e9c35aad2e1877de42527ee732a6cf253865.

--- src/com/android/settings/security/screenlock/ScreenLockSettings.java
+++ src/com/android/settings/security/screenlock/ScreenLockSettings.java
@@ -82,8 +82,6 @@ public class ScreenLockSettings extends DashboardFragment
         controllers.add(new PinScramblePreferenceController(
                 context, MY_USER_ID, lockPatternUtils));
         controllers.add(new FingerprintUnlockPreferenceController(context));
-        controllers.add(new QuickUnlockPreferenceController(
-                context, MY_USER_ID, lockPatternUtils));
         controllers.add(new OwnerInfoPreferenceController(context, parent));
         return controllers;
     }
